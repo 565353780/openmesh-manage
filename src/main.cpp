71c444c3aee15b50d985c0f6ff8d6d7e0afce8da
chLi <565353780@qq.com>
2024-09-26 19:31:58 +0800

    finish auto compile and include openmesh

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <iostream>
+
+int main() {
+  std::cout << "Run!" << std::endl;
+  return 0;
+}
